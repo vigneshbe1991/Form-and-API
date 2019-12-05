d15ccb16b809772b787f6b70d44c8db93cade87f
unknown <bvigneshece@gmail.com>
2019-12-05 11:16:00 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+// import Form from './Components/Form';
+// import Validation from './Components/Validation';
+// import Post from './Components/Post';
+import Get from './Components/Get';
+
+function App() {
+  return (
+    <div className="App">
+      <Get/>
+    </div>
+  );
+}
+
+export default App;
